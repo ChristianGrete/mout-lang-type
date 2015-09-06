cc10ae74032f4fb851e257b0f618579a3fbb36e9
Christian Grete <webmaster@christiangrete.com>
2015-09-06 14:37:43 +0200

    release(v0.0.1): distribute

---
+++ dist/lang/typeOf.js
@@ -0,0 +1,30 @@
+define(
+  [
+    'mout/lang/kindOf'
+  ]
+  function ( kindOf ) {
+    var
+      _prototype = String.prototype,
+      _charAt = _prototype.charAt,
+      _slice = _prototype.slice,
+      _toLowerCase = _prototype.toLowerCase;
+
+    return function typeOf ( $mixed ) {
+        var
+          _kind = kindOf( $mixed );
+
+        return (
+            _toLowerCase.call(
+              _charAt.call(
+                _kind,
+                0
+              )
+            )
+              + _slice.call(
+                  _kind,
+                  1
+                )
+          );
+      };
+  }
+);
