aa3ab395eb01cfaa07545b1793dc865a0d0c3423
Christian Grete <webmaster@christiangrete.com>
2015-09-07 17:57:56 +0200

    release(v0.1.0): distribute

---
+++ dist/amd/lang/typeOf.js
@@ -0,0 +1,64 @@
+define(
+  function () {
+    var
+      _builtInTag,
+      _builtInTags = [
+          'Arguments',
+          'Array',
+          'Boolean',
+          'Date',
+          'Error',
+          'Function',
+          'Null',
+          'Number',
+          'Object',
+          'RegExp',
+          'String',
+          'Undefined'
+        ],
+      _conversionObject = {},
+      _index = 0,
+      _length = _builtInTags.length,
+      _prototype = String.prototype,
+      _charAt = _prototype.charAt,
+      _slice = _prototype.slice,
+      _toLowerCase = _prototype.toLowerCase,
+      _toString = _conversionObject.toString;
+
+    for( ; _index < _length; _index ++ ) {
+      _builtInTag = _builtInTags[ _index ],
+
+      _conversionObject[
+        '[object '
+          + _builtInTag
+          + ']'
+      ]
+        = _toLowerCase.call(
+            _charAt.call(
+              _builtInTag,
+              0
+            )
+          )
+            + _slice.call(
+                _builtInTag,
+                1
+              );
+    }
+
+    return function typeOf ( $mixed ) {
+        if( $mixed == null ) {
+          return $mixed + '';
+        }
+
+        return (
+            typeof $mixed === 'object'
+              || typeof $mixed === 'function' ?
+                (
+                  _conversionObject[ _toString.call($mixed) ]
+                    || 'object'
+                ) :
+                  typeof $mixed
+          );
+      };
+  }
+);
