92c44cc721d0c50dc1d6e385645be9cacb3a614f
Christian Grete <webmaster@christiangrete.com>
2015-09-09 23:20:07 +0200

    release(v0.3.1): distribute

---
+++ dist/amd/lang.js
@@ -0,0 +1,8 @@
+define(
+  function ( require ) {
+    return {
+        isType: require('./lang/isType'),
+        typeOf: require('./lang/typeOf')
+      };
+  }
+);
---
+++ dist/cjs/lang.js
@@ -0,0 +1,6 @@
+
+    module.exports = {
+        isType: require('./lang/isType'),
+        typeOf: require('./lang/typeOf')
+      };
+  
