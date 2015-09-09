bbb1f41b254eba1cfe8b83cf88bf97a887315c09
Christian Grete <webmaster@christiangrete.com>
2015-09-10 00:49:43 +0200

    chore(*): make verbose

--- bin/release.sh
+++ bin/release.sh
@@ -4,24 +4,24 @@ cd "$( dirname "$0" )/..";
 
 echo "\r\nPlease specify the version sequence to bump (e.g. major, minor or patch):";
 
-read version && grunt bump:$version;
+read version && grunt bump:$version --verbose;
 
 git checkout master && git merge develop;
 
 hash="$( git rev-parse --verify HEAD )";
 
-grunt modify_json:manifests && grunt build;
+grunt modify_json:manifests --verbose && grunt build --verbose;
 
 git add {bower,package}.json && git add dist;
 
-grunt exec:commit && git push origin master --force;
+grunt exec:commit --verbose && git push origin master --force;
 
 echo "\r\nPlease enter a short tag message as a description for this release:";
 
-read tagMessage && grunt exec:tag --tagMessage="$tagMessage";
+read tagMessage && grunt exec:tag --tagMessage="$tagMessage" --verbose;
 
 git push origin --tags && npm publish ./;
 
-grunt clean;
+grunt clean --verbose;
 
 git reset $hash --hard && git checkout develop;
