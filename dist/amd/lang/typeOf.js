d7cfd0e831f65c8b4ca1549a786ecf665622e6f5
Christian Grete <webmaster@christiangrete.com>
2015-09-09 15:38:45 +0200

    release(v0.2.1): distribute

---
+++ dist/amd/lang/typeOf.js
@@ -0,0 +1,83 @@
+define(
+  function () {
+    var
+      _argumentsIsObject,
+      _builtInTag,
+      _builtInTags = [
+          'Arguments',
+          'Array',
+          'Boolean',
+          'Date',
+          'Error',
+          'Function',
+          'Number',
+          'Object',
+          'RegExp',
+          'String'
+        ],
+      _conversionObject = {},
+      _hasOwnProperty,
+      _index = 0,
+      _isArguments,
+      _length = _builtInTags.length,
+      _prototype = String.prototype,
+      _charAt = _prototype.charAt,
+      _slice = _prototype.slice,
+      _toLowerCase = _prototype.toLowerCase,
+      _toString = _conversionObject.toString;
+
+    for( ; _index < _length; _index ++ ) {
+      _builtInTag = _builtInTags[ _index ],
+
+      _conversionObject[
+        '[object '
+          + _builtInTag
+          + ']'
+      ]
+        = _toLowerCase.call(
+            _charAt.call(
+              _builtInTag,
+              0
+            )
+          )
+            + _slice.call(
+                _builtInTag,
+                1
+              );
+    }
+
+    if(
+      _argumentsIsObject = _conversionObject[ _toString.call(arguments) ] !== 'arguments'
+    ) {
+      _hasOwnProperty = _conversionObject.hasOwnProperty,
+
+      _isArguments = function ( $mixed ) {
+          return (
+              _hasOwnProperty.call(
+                $mixed,
+                'callee'
+              ) ?
+                'arguments' :
+                  false
+            );
+        };
+    }
+
+    return function typeOf ( $mixed ) {
+        if( $mixed == null ) {
+          return $mixed + '';
+        }
+
+        return (
+            typeof $mixed === 'object'
+              || typeof $mixed === 'function' ?
+                (
+                  ( _argumentsIsObject && _isArguments() )
+                    || _conversionObject[ _toString.call($mixed) ]
+                      || 'object'
+                ) :
+                  typeof $mixed
+          );
+      };
+  }
+);
