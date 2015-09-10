445eb39d09990e8208ab81d58b48bb945fde3b02
Christian Grete <webmaster@christiangrete.com>
2015-09-11 00:53:38 +0200

    docs(src): add jsdoc comments - rename arguments to match jsdoc wordings - fix internal `_isArguments` call

--- src/lang.js
+++ src/lang.js
@@ -1,5 +1,22 @@
+/**
+ * @author Christian Grete <webmaster@christiangrete.com>
+ * @copyright Christian Grete 2015
+ * @file Provides all [lang]{@link module:mout-lang-type.lang} utilities
+ * @license MIT
+ */
+
 define(
   function ( require ) {
+
+    /**
+     * The namespace/object containing the <code>lang</code> utilities
+     * @author Christian Grete <webmaster@christiangrete.com>
+     * @license MIT
+     * @namespace {object} module:mout-lang-type.lang
+     * @property {function} isType Checks whether a value is of a type
+     * @property {function} typeOf Gets the type of a value
+     */
+
     return {
         isType: require('./lang/isType'),
         typeOf: require('./lang/typeOf')
--- src/lang/typeOf.js
+++ src/lang/typeOf.js
@@ -1,3 +1,10 @@
+/**
+ * @author Christian Grete <webmaster@christiangrete.com>
+ * @copyright Christian Grete 2015
+ * @file Provides the [typeOf]{@link module:mout-lang-type.lang.typeOf} utility
+ * @license MIT
+ */
+
 define(
   function () {
     var
@@ -47,14 +54,15 @@ define(
     }
 
     if(
-      _argumentsIsObject = _conversionObject[ _toString.call(arguments) ] !== 'arguments'
+      _argumentsIsObject
+        = _conversionObject[ _toString.call(arguments) ] !== 'arguments'
     ) {
       _hasOwnProperty = _conversionObject.hasOwnProperty,
 
-      _isArguments = function ( $mixed ) {
+      _isArguments = function ( $value ) {
           return (
               _hasOwnProperty.call(
-                $mixed,
+                $value,
                 'callee'
               ) ?
                 'arguments' :
@@ -63,20 +71,30 @@ define(
         };
     }
 
-    return function typeOf ( $mixed ) {
-        if( $mixed == null ) {
-          return $mixed + '';
+    /**
+     * Returns the type of the passed value
+     * @author Christian Grete <webmaster@christiangrete.com>
+     * @function module:mout-lang-type.lang.typeOf
+     * @license MIT
+     * @param {mixed} $value The <code>object</code> or <code>primitive</code> whose type is to be returned
+     * @returns {string} A <code>string</code> indicating the type of the passed argument
+     * @summary Gets the type of a value
+     */
+
+    return function typeOf ( $value ) {
+        if( $value == null ) {
+          return $value + '';
         }
 
         return (
-            typeof $mixed === 'object'
-              || typeof $mixed === 'function' ?
+            typeof $value === 'object'
+              || typeof $value === 'function' ?
                 (
-                  ( _argumentsIsObject && _isArguments() )
-                    || _conversionObject[ _toString.call($mixed) ]
+                  ( _argumentsIsObject && _isArguments($value) )
+                    || _conversionObject[ _toString.call($value) ]
                       || 'object'
                 ) :
-                  typeof $mixed
+                  typeof $value
           );
       };
   }
