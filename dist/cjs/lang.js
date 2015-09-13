6260ac1ef6337c96c7f973223b9209a6e67543d8
Christian Grete <webmaster@christiangrete.com>
2015-09-13 02:13:20 +0200

    release(v0.4.1): distribute

---
+++ dist/cjs/lang.js
@@ -0,0 +1,23 @@
+/**
+ * @author Christian Grete <webmaster@christiangrete.com>
+ * @copyright Christian Grete 2015
+ * @file Provides all [lang]{@link module:mout-lang-type.lang} utilities
+ * @license MIT
+ */
+
+
+
+    /**
+     * The namespace/object containing the <code>lang</code> utilities
+     * @author Christian Grete <webmaster@christiangrete.com>
+     * @license MIT
+     * @namespace {object} module:mout-lang-type.lang
+     * @property {function} isType Checks whether a value is of a type
+     * @property {function} typeOf Gets the type of a value
+     */
+
+    module.exports = {
+        isType: require('./lang/isType'),
+        typeOf: require('./lang/typeOf')
+      };
+  
