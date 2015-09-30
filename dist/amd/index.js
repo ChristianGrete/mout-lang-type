effc5c99af34489e5b459c1217441eeef223c722
Christian Grete <webmaster@christiangrete.com>
2015-09-30 12:44:37 +0200

    release(v0.4.4): distribute

---
+++ dist/amd/index.js
@@ -0,0 +1,27 @@
+/**
+ * @author Christian Grete <webmaster@christiangrete.com>
+ * @copyright Christian Grete 2015
+ * @file Provides the [mout-lang-type]{@link module:mout-lang-type} module
+ * @license MIT
+ */
+
+define(
+  function ( require ) {
+
+    /**
+     * The module/object containing the module’s exports
+     * @author Christian Grete <webmaster@christiangrete.com>
+     * @license MIT
+     * @module mout-lang-type
+     * @property {object} lang Contains all <code>lang</code> utilities
+     * @property {function} lang.isType Checks whether a value is of a type
+     * @property {function} lang.typeOf Gets the type of a value
+     * @property {string} VERSION Indicates the version number of the module
+     */
+
+    return {
+        'lang': require('./lang'),
+        'VERSION': '0.4.4'
+      };
+  }
+);
---
+++ dist/cjs/index.js
@@ -0,0 +1,25 @@
+/**
+ * @author Christian Grete <webmaster@christiangrete.com>
+ * @copyright Christian Grete 2015
+ * @file Provides the [mout-lang-type]{@link module:mout-lang-type} module
+ * @license MIT
+ */
+
+
+
+    /**
+     * The module/object containing the module’s exports
+     * @author Christian Grete <webmaster@christiangrete.com>
+     * @license MIT
+     * @module mout-lang-type
+     * @property {object} lang Contains all <code>lang</code> utilities
+     * @property {function} lang.isType Checks whether a value is of a type
+     * @property {function} lang.typeOf Gets the type of a value
+     * @property {string} VERSION Indicates the version number of the module
+     */
+
+    module.exports = {
+        'lang': require('./lang'),
+        'VERSION': '0.4.4'
+      };
+  
