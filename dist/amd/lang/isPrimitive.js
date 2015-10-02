c76d4e35c4ed37e80ae47954925721df0fc59a51
Christian Grete <webmaster@christiangrete.com>
2015-10-02 16:12:36 +0200

    release(v0.5.0): distribute

---
+++ dist/amd/lang/isPrimitive.js
@@ -0,0 +1,32 @@
+/**
+ * @author Christian Grete <webmaster@christiangrete.com>
+ * @copyright &copy; 2015 Christian Grete
+ * @file Provides the [isPrimitive]{@link module:mout-lang-type.lang.isPrimitive} utility
+ * @license MIT
+ */
+
+define(
+  function () {
+
+    /**
+     * Returns whether the passed value is of a primitive data type
+     * @author Christian Grete <webmaster@christiangrete.com>
+     * @function module:mout-lang-type.lang.isPrimitive
+     * @license MIT
+     * @param {mixed} $value The <code>object</code> or <code>primitive</code> whose data type is to be checked
+     * @returns {boolean} A <code>boolean</code> indicating whether the passed argument is a primitive value
+     * @see [mout/lang/isPrimitive]{@link https://github.com/mout/mout/commit/1b31347}
+     * @summary Checks whether a value is primitive
+     */
+
+    return function isPrimitive ( $value ) {
+        return (
+            $value === null
+              || (
+                typeof $value !== 'object'
+                  && typeof $value !== 'function'
+              )
+          );
+      };
+  }
+);
---
+++ dist/cjs/lang/isType.js
@@ -0,0 +1,25 @@
+/**
+ * @author Christian Grete <webmaster@christiangrete.com>
+ * @copyright &copy; 2015 Christian Grete
+ * @file Provides the [isType]{@link module:mout-lang-type.lang.isType} utility
+ * @license MIT
+ */
+
+var typeOf = require('./typeOf');
+
+    /**
+     * Returns whether the passed value is of the specified type
+     * @author Christian Grete <webmaster@christiangrete.com>
+     * @function module:mout-lang-type.lang.isType
+     * @license MIT
+     * @param {mixed} $value The <code>object</code> or <code>primitive</code> whose type is to be compared
+     * @param {string} $type The <code>string</code> indicating the type to be compared against
+     * @requires module:mout-lang-type.lang.typeOf
+     * @returns {boolean} A <code>boolean</code> indicating the result of the type comparison
+     * @summary Checks whether a value is of a type
+     */
+
+    module.exports = function isType ( $value, $type ) {
+        return typeOf( $value ) === $type;
+      };
+  
