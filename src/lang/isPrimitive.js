b6fee0d9f7a4fa6def7bd5d68b8ac708d618fa2e
Christian Grete <webmaster@christiangrete.com>
2015-11-12 18:14:53 +0100

    docs(src): update jsdoc comments

--- src/lang/isPrimitive.js
+++ src/lang/isPrimitive.js
@@ -1,6 +1,7 @@
 /**
+ * @author Garrick Cheung <garrick@garrickcheung.com>
  * @author Christian Grete <webmaster@christiangrete.com>
- * @copyright &copy; 2015 Christian Grete
+ * @copyright &copy; 2014 Garrick Cheung &amp; Christian Grete
  * @file Provides the [isPrimitive]{@link module:mout-lang-type.lang.isPrimitive} utility
  * @license MIT
  */
@@ -10,11 +11,12 @@ define(
 
     /**
      * Returns whether the passed value is of a primitive data type
+     * @author Garrick Cheung <garrick@garrickcheung.com>
      * @author Christian Grete <webmaster@christiangrete.com>
      * @function module:mout-lang-type.lang.isPrimitive
      * @license MIT
      * @param {mixed} $value The <code>object</code> or <code>primitive</code> whose data type is to be checked
-     * @returns {boolean} A <code>boolean</code> indicating whether the passed argument is a primitive value
+     * @returns {boolean} A <code>boolean</code> indicating whether the passed argument has a primitive value
      * @see [mout/lang/isPrimitive]{@link https://github.com/mout/mout/commit/1b31347}
      * @summary Checks whether a value is primitive
      */
