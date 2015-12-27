25145373bf6822f8832c2582e13d41e5362f70a3
Christian Grete <webmaster@christiangrete.com>
2015-12-27 20:54:41 +0100

    feat(lang): add `GLOBAL` constant

---
+++ src/lang/GLOBAL.js
@@ -0,0 +1,22 @@
+/**
+ * @author Christian Grete <webmaster@christiangrete.com>
+ * @copyright &copy; 2015 Christian Grete
+ * @file Provides the [GLOBAL]{@link module:mout-lang-type.lang.GLOBAL} constant
+ * @license MIT
+ */
+
+define(
+  function () {
+
+    /**
+     * A reference to the global scope object
+     * @author Christian Grete <webmaster@christiangrete.com>
+     * @constant {object} module:mout-lang-type.lang.GLOBAL
+     * @default <code>global</code> or <code>window</code>
+     * @license MIT
+     * @summary References the global object
+     */
+
+    return (1,eval)('this') // jshint ignore:line
+  }
+)
